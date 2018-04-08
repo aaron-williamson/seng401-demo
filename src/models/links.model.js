438364a48f19cf8f69923ba82aa72a7a5420ce3e
Aaron Williamson <guitarfanman@gmail.com>
2018-04-08 15:19:13 -0600

    feathers generate service

---
+++ src/models/links.model.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-console */
+
+// links-model.js - A KnexJS
+//
+// See http://knexjs.org/
+// for more of what you can do here.
+module.exports = function (app) {
+  const db = app.get('knexClient');
+  const tableName = 'links';
+  db.schema.hasTable(tableName).then(exists => {
+    if(!exists) {
+      db.schema.createTable(tableName, table => {
+        table.increments('id');
+        table.string('link');
+      })
+        .then(() => console.log(`Created ${tableName} table`))
+        .catch(e => console.error(`Error creating ${tableName} table`, e));
+    }
+  });
+
+  return db;
+};
