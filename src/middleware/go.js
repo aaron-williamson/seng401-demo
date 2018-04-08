57f3d0028f85ea6b0345ae37b51ec1bd5fa5fccf
Aaron Williamson <guitarfanman@gmail.com>
2018-04-08 17:08:25 -0600

    Add logging to go middleware

--- src/middleware/go.js
+++ src/middleware/go.js
@@ -1,12 +1,15 @@
 module.exports = function (options = {}) {
   return function go(req, res, next) {
     const errors = require('@feathersjs/errors');
+    const logger = require('winston');
     const app = req.app;
     const links = app.service('links');
+    logger.info((new Date).toISOString() + ' "' + req.method + ' ' + req.originalUrl + '"');
 
     links.get(req.params.id)
       .then(link => {
         const url = link.link;
+        logger.info('Found link ' + url + ' successfully, redirecting');
         res.redirect(url);
       })
       .catch(err => {
